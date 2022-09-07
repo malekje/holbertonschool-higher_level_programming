45af986166e73b9ba6f1fc0a0ad43e3429cc1083
Malek Mayeh <loukatankionline@gmail.com>
2022-09-07 13:52:30 +0100

    The last digit

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = int((number)[-1])
+last_digit = int(repr(number - 1)
 lessthen = "and is less than 6 and not 0"
 if number < 0:
     last_digit = -last_digit
