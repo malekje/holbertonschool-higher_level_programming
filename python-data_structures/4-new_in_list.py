00519d14f1bc549f4cb2a4e4176933da092a1ecb
Malek Mayeh <loukatankionline@gmail.com>
2022-09-14 12:32:07 +0100

    replace an element without modifying

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    sec_list = my_list.copy()
+    if idx < 0 or idx > len(my_list) - 1:
+        return my_list
+    sec_list[idx] = element
+    return sec_list
