339f0348e7111c15734c441d74659b8855572945
Malek Mayeh <loukatankionline@gmail.com>
2022-09-20 12:09:24 +0100

    area of square

---
+++ python-classes/3-square.py
@@ -0,0 +1,18 @@
+"""class square"""
+
+
+class Square:
+    """
+    class Square
+    Private instance attribute: size
+
+    """
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+    def area(self):
+        return self.__size * self.__size
