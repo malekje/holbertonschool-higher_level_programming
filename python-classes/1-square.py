a1acbe00c49894fcc8e44e90512cde80e7e8e693
Malek Mayeh <loukatankionline@gmail.com>
2022-09-20 11:58:44 +0100

    square with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""class Square"""
+
+
+class Square:
+    """
+    class Square
+    Private instance attribute: size
+    
+    """
+    def __init__(self, size):
+        self.__size = size
