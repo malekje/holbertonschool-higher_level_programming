e3e8abaa9a12bd8aeb94fc2994168cab10a58b41
Malek Mayeh <loukatankionline@gmail.com>
2022-09-28 14:30:57 +0100

    improve geo

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""area(self)"""
+
+
+class BaseGeometry:
+    """area(self)"""
+
+    def area(self):
+        """area(self)"""
+        raise Exception("area() is not implemented")
