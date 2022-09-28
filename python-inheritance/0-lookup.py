5263ce33a4fd2ccc74b1f4641db04b6261c09ef0
Malek Mayeh <loukatankionline@gmail.com>
2022-09-28 13:14:41 +0100

    Return the list

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -2,8 +2,9 @@
 """
 returns the list of available attributes and methods of an object
 """
+
 def lookup(obj):
-    """ 
+    """
     returns the list of available attributes and methods of an object
-     """
+    """
     return dir(obj)
