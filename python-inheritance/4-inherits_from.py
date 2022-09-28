ce767aa2975b5a79ca2138fe42e9faa30e398bc0
Malek Mayeh <loukatankionline@gmail.com>
2022-09-28 14:20:24 +0100

    class that inherited

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""True if the object is an instance of a class that inherited"""
+
+
+def inherits_from(obj, a_class):
+    """True if the object is an instance of a class that inherited"""
+    if isinstance(obj, a_class):
+        return True
