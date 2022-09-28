f3b403a1d0e9362a021ef35c4a2f27407adca1a8
Malek Mayeh <loukatankionline@gmail.com>
2022-09-28 13:56:32 +0100

    My list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+inherits from list
+"""
+
+
+class MyList(list):
+    """
+    inherits from list
+    """
+    def print_sorted(self):
+        """
+        print list
+        """
+        print(sorted(self))
