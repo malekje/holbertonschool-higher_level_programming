4fd096b1765adcd03e5b3dfb51aa5f6934eeb22f
Malek Mayeh <loukatankionline@gmail.com>
2022-09-28 14:16:00 +0100

    check obj isinstance

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""True if the object is an instance of, or if the object is an instance of a class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """True if the object is an instance of, or if the object is an instance of a class"""
+    isinstance(obj, a_class)
