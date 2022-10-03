79f4895e9f8a122138ce1bdd7df6fb0593682ad1
Malek Mayeh <loukatankionline@gmail.com>
2022-10-03 13:56:08 +0100

    writes an obj to txt file

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """writes an Object to a text file"""
+import json
 
 
 def save_to_json_file(my_obj, filename):
     """writes an Object to a text file"""
     with open(filename, "w") as file:
-        return(file.write(my_obj))
+        json.dump(my_obj, file)
