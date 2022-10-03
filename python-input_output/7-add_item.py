492c7c3b0add64b43215e93e333e4e1c0f8ef0a2
Malek Mayeh <loukatankionline@gmail.com>
2022-10-03 14:20:49 +0100

    load add save

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""
-adds all arguments to a Python list
-"""
+"""add arguments to python list"""
+
 
 import sys
+
+
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
     load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
