8beb57e9ce65ccff3d05ff10848ed0a5efdb5a76
Malek Mayeh <loukatankionline@gmail.com>
2022-10-03 14:15:54 +0100

    load add save

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-""" 
-7. Load, add, save
+"""
 adds all arguments to a Python list
 """
+
 from sys import argv
-
-
 save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
 
