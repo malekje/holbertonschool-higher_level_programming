3296621e9a0e71787f48caf377fa295f3ab57d0d
Malek Mayeh <loukatankionline@gmail.com>
2022-10-03 14:22:04 +0100

    load add save

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-"""add arguments to python list"""
+"""
+add arguments to python list
+You must use your function save_to_json_file from 5-save_to_json_file.py
+You must use your function load_from_json_file from 6-load_from_json_file.py
+The list must be saved as a JSON representation in a file named add_item.json
+"""
 
 
 import sys
