48f633d709bb8e5496ad46547d5d3a9cd57ad1c2
Malek Mayeh <loukatankionline@gmail.com>
2022-10-03 14:19:43 +0100

    load add save

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,9 +3,10 @@
 adds all arguments to a Python list
 """
 
-from sys import argv
-save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
+import sys
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
 
 list = "add_item.json"
 
@@ -14,6 +15,5 @@ try:
 except:
     file = []
 
-for arg in argv[1:]:
-    file.append(arg)
-save_to_json_file(file, "add_item.json")
+    file.extend(sys.argv[1:])
+save_to_json_file(file, list)
