fc50a5100cf76a4791cac3a52b40c492afb2be30
Malek Mayeh <loukatankionline@gmail.com>
2022-10-03 13:28:40 +0100

    write file UTF8

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""writing file function"""
+
+
+def write_file(filename="", text=""):
+    """writing file function"""
+    with open(filename, "w",encoding="utf-8") as file:
+        print(file.write(), end="")
