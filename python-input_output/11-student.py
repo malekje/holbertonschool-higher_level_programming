b5991dfa4fb45fb922661e68b7f5ca85f735ac19
Malek Mayeh <loukatankionline@gmail.com>
2022-10-03 15:01:20 +0100

    student

---
+++ python-input_output/11-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""class student"""
+
+
+class Student:
+    """class student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """returns dict"""
+        return self.__dict__
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation"""
+        if attrs is None:
+            return self.__dict__
+        new_dict = {}
+        for a in attrs:
+            try:
+                new_dict[a] = self.__dict__[a]
+            except:
+                pass
+        return new_dict
+    def reload_from_json(self, json):
+        """transfer the attributes"""
+        for n, m in json.items():
+            setattr(self, n, m)
