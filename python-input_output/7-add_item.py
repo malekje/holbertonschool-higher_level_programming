10b15286081eaa4a7b44c4a956b91d24864bc8bc
Malek Mayeh <loukatankionline@gmail.com>
2022-10-05 15:48:37 +0100

    fix

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
-""" input output """
+""""adds all arguments to a Python list"""
 
 import sys
+from unicodedata import name
 
 if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__(
+        '6-load_from_json_file').load_from_json_file
 
-    save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
-    load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
+    name = "add_item.json"
+    arg = sys.argv[1:]
 
-list = "add_item.json"
+    try:
+        a = load_from_json_file(name)
 
-try:
-    file = load_from_json_file(list)
-except:
-    file = []
+    except FileNotFoundError:
+        a = []
 
-    file.extend(sys.argv[1:])
-save_to_json_file(file, list)
+    a.extend(arg)
+    save_to_json_file(a, name)
