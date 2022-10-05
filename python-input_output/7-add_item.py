9c8d71642ea7413f0122dc6aeed34d9a230d6ee7
Malek Mayeh <loukatankionline@gmail.com>
2022-10-05 15:43:47 +0100

    fix

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-"""add arguments to python list"""
-
+""" input output """
 
 import sys
 
-
 if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
-    load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__(
+        '6-load_from_json_file').load_from_json_file
 
-list = "add_item.json"
+    filename = "add_item.json"
+    arg = sys.argv[1:]
 
-try:
-    file = load_from_json_file(list)
-except:
-    file = []
+    try:
+        my_list = load_from_json_file(filename)
 
-    file.extend(sys.argv[1:])
-save_to_json_file(file, list)
+    except FileNotFoundError:
+        my_list = []
+
+    my_list.extend(arg)
+    save_to_json_file(my_list, filename)
