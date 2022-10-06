de8c72f082c711206b59d688dc989f714c3d5a49
Malek Mayeh <loukatankionline@gmail.com>
2022-10-06 13:28:30 +0100

    json string

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -5,6 +5,7 @@ base class
 """
 import json
 
+
 class Base:
     """
     base class
@@ -26,3 +27,18 @@ class Base:
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """write the JSON string to a file"""
+        filename = cls.__name__ + ".json"
+        list_dic = []
+
+        if list_objs is None:
+            with open(filename, "w") as f:
+                f.write(cls.to_json_string(list_dic))
+            return
+        for obj in list_objs:
+            list_dic.append(obj.to_dictionary())
+        with open(filename, "w") as f:
+            f.write(cls.to_json_string(list_dic))
