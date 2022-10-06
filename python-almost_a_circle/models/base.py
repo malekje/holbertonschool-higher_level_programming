17ad8332248485284cd7b78c80367293dfd766e4
Malek Mayeh <loukatankionline@gmail.com>
2022-10-06 13:31:04 +0100

    json string

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -50,3 +50,13 @@ class Base:
             return []
         else:
             return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """returns an instance with attributes"""
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        else:
+            dummy = cls(1)
+        dummy.update(**dictionary)
+        return(dummy)
