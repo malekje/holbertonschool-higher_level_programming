26d9d683dfb29711788faff62755394c59f23e59
Malek Mayeh <loukatankionline@gmail.com>
2022-10-06 12:28:41 +0100

    fix

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -17,48 +17,55 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """width"""
         return self.__width
+
+    @property
+    def height(self):
+        """height"""
+        return self.__height
+
+    @property
+    def x(self):
+        """x"""
+        return self.__x
+
+    @property
+    def y(self):
+        """y"""
+        return self.__y
     
     @width.setter
     def width(self, value):
-        if not isinstance(value, int):
+        """setter width"""
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
         self.__width = value
 
-    @property
-    def height(self):
-        return self.__height
-    
     @height.setter
     def height(self, value):
-        if not isinstance(value, int):
+        """setter height"""
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
         self.__height = value
-    
-    @property
-    def x(self):
-        return self.__x
-    
+
     @x.setter
     def x(self, value):
-        if not isinstance(value, int):
+        """setter x"""
+        if type(value) is not int:
             raise TypeError("x must be an integer")
         if value < 0:
             raise ValueError("x must be >= 0")
         self.__x = value
 
-    @property
-    def y(self):
-        """func Y"""
-        return self.__y
-    
+    @y.setter
     def y(self, value):
-        """func Y"""
-        if not isinstance(value, int):
+        """setter y"""
+        if type(value) is not int:
             raise TypeError("y must be an integer")
         if value < 0:
             raise ValueError("y must be >= 0")
