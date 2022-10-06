2e37961cbf13dd67cad8f010217f0831c35f35e4
Malek Mayeh <loukatankionline@gmail.com>
2022-10-06 13:10:52 +0100

    square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -10,3 +10,20 @@ class Square(Rectangle):
         """init"""
         super().__init__(size, size, x, y, id)
         self.size = size
+
+    @property
+    def size(self):
+        """size"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """size"""
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """Prints Square"""
+        return "[{:s}] ({:d}) {:d}/{:d} - {:d}".format(
+            self.__class__.__name__, self.id, self.x, self.y,
+            self.size)
