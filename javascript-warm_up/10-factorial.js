f4af1c3774ef960f7ab3da0069106a58a8ac4b62
Malek Mayeh <loukatankionline@gmail.com>
2022-12-08 17:15:49 +0100

    factorial

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const firstArg = process.argv[2];
+const num = parseInt(firstArg);
+
+function factorial (n) {
+    if ((isNaN(n)) || (n === 1)) {
+      return 1;
+    } else {
+      return n * factorial(n - 1);
+    }
+  }
+  console.log(factorial(num));
