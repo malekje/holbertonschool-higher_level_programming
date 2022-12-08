9c3396f487eb80640da6df890ed94d7fb49eb252
Malek Mayeh <loukatankionline@gmail.com>
2022-12-08 16:22:28 +0100

    loop to langs

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-var lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (var i = 0; i < lines.length; i++) {
   console.log(lines[i]);
