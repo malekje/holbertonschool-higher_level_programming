461281f585ebd5b17279784f35edd0a4118fce91
Malek Mayeh <loukatankionline@gmail.com>
2022-12-08 18:09:04 +0100

    increment obj

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -2,18 +2,14 @@
 const myObject = {
     type: 'object',
     value: 12
-  };
-  console.log(myObject);
-function incr(x) {
-    x = parseInt(x);
-    if (isNaN(x)) {
-      return 0;
-    }
-    return x + 1;
-  }
-  myObject.incr();
-  console.log(myObject);
-  myObject.incr();
-  console.log(myObject);
-  myObject.incr();
-  console.log(myObject);
+};
+console.log(myObject);
+myObject.incr = function () {
+    myObject.value++;
+};
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
