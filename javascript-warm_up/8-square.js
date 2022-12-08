34d17bad74575b170e1f8ffc43bba12da67f720f
Malek Mayeh <loukatankionline@gmail.com>
2022-12-08 16:40:47 +0100

    square

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const firstArg = process.argv[2];
+const size = parseInt(firstArg);
+
+
+if (!isNaN(size)) {
+  for (var i = 0; i < size; i++) {
+    var row = "";
+    for (var j = 0; j < size; j++) {
+      row += "#";
+    }
+    console.log(row);
+  }
+} else {
+  console.log('Missing size');
+}
