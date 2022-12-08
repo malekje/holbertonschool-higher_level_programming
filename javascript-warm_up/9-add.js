67e2eeefc68b3d9105f849d04be410800a881a2c
Malek Mayeh <loukatankionline@gmail.com>
2022-12-08 16:52:19 +0100

    add

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -3,7 +3,7 @@ const firstArg = process.argv[2];
 const secondArg = process.argv[3];
 
 function add (a, b) {
-    return parseInt(a) + parseInt(b);
-  }
-  
+  return parseInt(a) + parseInt(b);
+}
+
   console.log(add(firstArg, secondArg));
