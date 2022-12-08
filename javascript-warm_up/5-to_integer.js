5e95956cda7c47d3b70f3f4ea0dc1e53d754cfa8
Malek Mayeh <loukatankionline@gmail.com>
2022-12-08 16:19:22 +0100

    an integer

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -4,8 +4,7 @@ const firstArg = process.argv[2];
 const firstArgInt = parseInt(firstArg);
 
 if (!isNaN(firstArgInt)) {
-  console.log("My number: " + firstArgInt);
+  console.log('My number: ' + firstArgInt);
 } else {
-
   console.log('Not a number');
 }
