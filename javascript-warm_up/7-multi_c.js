a3db0daa2e60b3c831e71e70a26de1c6339c0544
Malek Mayeh <loukatankionline@gmail.com>
2022-12-08 16:31:35 +0100

    i love c

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -3,11 +3,10 @@
 const firstArg = process.argv[2];
 const repeatCount = parseInt(firstArg);
 
-if(!isNaN(repeatCount)) {
-
+if (!isNaN(repeatCount)) {
   for (let i = 0; i < repeatCount; i++) {
     console.log('C is fun');
-  }
+}
 } else {
   console.log('Missing number of occurrences');
 }
