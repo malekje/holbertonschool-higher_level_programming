95d65f2762296fdb6f661ba62ee2172cf52cd35d
Malek Mayeh <loukatankionline@gmail.com>
2022-12-08 16:36:52 +0100

    c is fun

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -6,7 +6,7 @@ const repeatCount = parseInt(firstArg);
 if (!isNaN(repeatCount)) {
   for (let i = 0; i < repeatCount; i++) {
     console.log('C is fun');
-}
+  }
 } else {
   console.log('Missing number of occurrences');
 }
