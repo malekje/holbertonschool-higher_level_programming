f2a06b15c1f34e1cf9cef83ceaa08c3e66bdbc35
Malek Mayeh <loukatankionline@gmail.com>
2022-12-08 17:58:35 +0100

    call me moby

--- javascript-warm_up/101-call_me_moby.js
+++ javascript-warm_up/101-call_me_moby.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 exports.callMeMoby = function (x, theFunction) {
-    let result = [];
-    for (let i = 0; i < x; i++) {
-        result.push(theFunction());
-    }
+  const result = [];
+  for (let i = 0; i < x; i++) {
+    result.push(theFunction());
+  }
     return result;
 }
