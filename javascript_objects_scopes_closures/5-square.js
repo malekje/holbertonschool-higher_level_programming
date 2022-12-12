d72a116992970346225412f14d9e20c0dcf0508f
Malek Mayeh <loukatankionline@gmail.com>
2022-12-12 14:25:10 +0100

    square

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 module.exports = class Square extends require('./4-rectangle.js') {
-constructor(size) {
+  constructor(size) {
     // Call the constructor of the superclass (Rectangle)
     // with the size argument as both the width and height
     super(size, size);
