6e8238123462721521a76c6c90617f2ef91d3c83
Malek Mayeh <loukatankionline@gmail.com>
2022-12-12 15:05:15 +0100

    square

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -2,9 +2,9 @@
 
 module.exports = class Square extends require('./5-square.js') {
 
-  charPrint(c = 'X') {
+  charPrint (c = 'X') {
     for (let i = 0; i < this.height; i++) {
       console.log(c.repeat(this.width));
     }
   }
-}
+};
