c32178b2702180089bdf3cd74c70feb0a709d1e9
Malek Mayeh <loukatankionline@gmail.com>
2022-12-14 12:45:47 +0100

    factor index

--- javascript_objects_scopes_closures/100-map.js
+++ javascript_objects_scopes_closures/100-map.js
@@ -1,12 +1,6 @@
 #!/usr/bin/node
-// msg - [Got]
-// [ 1, 2, 3, 4, 5 ]
-// [ 0, 2, 6, 12, 20 ]
-// [Expected]
-// [ 0, 1, 2, 3 ]
-// [ 0, 1, 4, 9 ]
-// const initialList = [1, 2, 3, 4, 5];
-// const newList = initialList.map((value, index) => value * index);
-// console.log(initialList); 
-// console.log(newList);
+
 exports.list = [1, 2, 3, 4, 5];
+const newList = list.map((value, index) => value * index);
+console.log(list); 
+console.log(newList);
