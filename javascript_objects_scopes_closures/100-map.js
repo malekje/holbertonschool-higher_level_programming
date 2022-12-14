55f09b7fe452b72c271c2c6484e349bd4e43c8bf
Malek Mayeh <loukatankionline@gmail.com>
2022-12-14 12:41:04 +0100

    factor index

--- javascript_objects_scopes_closures/100-map.js
+++ javascript_objects_scopes_closures/100-map.js
@@ -3,5 +3,5 @@
 const initialList = [1, 2, 3, 4, 5];
 const newList = initialList.map((value, index) => value * index);
 // Print the initial and new arrays
-console.log('Initial list:', initialList); 
-console.log('New list:', newList);
+console.log(initialList); 
+console.log(newList);
