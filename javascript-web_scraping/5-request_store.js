88aacada937961cb48b7d52cb9f540615ac039e6
Malek Mayeh <loukatankionline@gmail.com>
2022-12-14 13:41:16 +0100

    Loripsum

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const request = require("request");
-const fs = require("fs");
+const request = require('request');
+const fs = require('fs');
 const url = process.argv[2];
 const filePath = process.argv[3];
 
@@ -9,10 +9,9 @@ request.get(url, (err, res, body) => {
     console.error(err);
     return;
   }
-  fs.writeFile(filePath, body, { encoding: "utf8" }, (err) => {
+  fs.writeFile(filePath, body, { encoding: 'utf8' }, (err) => {
     if (err) {
       console.error(err);
-      return;
     }
   });
 });
