a4c63b68572d52f7567cd4415fa9844e8093a2a4
Malek Mayeh <loukatankionline@gmail.com>
2022-12-14 13:37:20 +0100

    fixing

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -10,7 +10,7 @@ request.get(API_URL, (err, res, body) => {
     return;
   }
   const data = JSON.parse(body);
-
   const movies = data.results.filter(movie => movie.characters.includes(`https://swapi-api.hbtn.io/api/people/${CHARACTER_ID}/`));
+
   console.log(`${movies.length}`);
 });
