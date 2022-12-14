71ce7d7a416763396a2ab09fc4904e877111986f
Malek Mayeh <loukatankionline@gmail.com>
2022-12-14 13:43:42 +0100

    how many completed

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2] || 'https://jsonplaceholder.typicode.com/todos';
+
+request.get(url, (err, res, body) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  const tasks = JSON.parse(body);
+  const completedTasksByUser = new Map();
+  tasks.forEach((task) => {
+    if (task.completed) {
+      const userId = task.userId;
+      if (!completedTasksByUser.has(userId)) {
+        completedTasksByUser.set(userId, 0);
+      }
+      completedTasksByUser.set(userId, completedTasksByUser.get(userId) + 1);
+    }
+  });
+  completedTasksByUser.forEach((count, userId) => {
+    console.log(`User ${userId} has completed ${count} tasks.`);
+  });
+});
